0157888ebcc3832473b42892d28cf58596813b8c
Liz G <87549753+hellvalleyskytrees@users.noreply.github.com>
2024-03-16 02:41:45 -0500

    Update script.js

--- tokens/Wormhole/script.js
+++ tokens/Wormhole/script.js
@@ -92,12 +92,12 @@ function onStart(store, helpers)
 {
  	if (store.gateCounter == undefined)
 	{
-		store.gateCounter = 0;	
+		store.gateCounter = 0;
 	}
 	
 	if (store.sequenceCounter == undefined)
 	{
-		store.sequenceCounter = 0;	
+		store.sequenceCounter = 0;
 	}
 }
 
