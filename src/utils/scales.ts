ae971d947f4985dcb1db1ef8ddbf8518b8fde0e6
mina <aliceminacarter@gmail.com>
2026-04-05 19:01:59 -0700

    typo

--- src/utils/scales.ts
+++ src/utils/scales.ts
@@ -3,7 +3,7 @@ export const modes = {
   "Harmonic Major": [0, 2, 4, 5, 7, 8, 11],
   Minor: [0, 2, 3, 5, 7, 8, 10],
   "Harmonic Minor": [0, 2, 3, 5, 7, 8, 11],
-  MelodicMinor: [0, 2, 3, 5, 7, 9, 11],
+  "Melodic Minor": [0, 2, 3, 5, 7, 9, 11],
   "Double Harmonic Minor": [0, 2, 3, 6, 7, 8, 11],
   "Double Harmonic Major": [0, 1, 4, 5, 7, 8, 11],
   Enigmatic: [0, 1, 4, 6, 8, 10, 11],
